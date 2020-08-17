38ff05db61af98694e8c7c835f7b2caec0349214
Yijun Ma <mayijun1203@gmail.com>
2020-08-16 22:47:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,55 +42,65 @@ console.log(c.title.second)
 console.log(c['title'])
 
 var d = {
-    name:'haha',
-    'title title':{
-        first:'a',
-        second:'b'
+    name: 'haha',
+    'title title': {
+        first: 'a',
+        second: 'b'
     }
 };
 console.log(d)
 
 
-function mm(mt){
-    var mf=function(x){
-        return mt*x;
+function mm(mt) {
+    var mf = function (x) {
+        return mt * x;
     };
     return mf
 }
-var m3=mm(3);
+var m3 = mm(3);
 console.log(m3(2))
 
-var e=10;
-var f=e;
-f=20;
+var e = 10;
+var f = e;
+f = 20;
 console.log(e)
 
-var g={x:10};
-var h=g;
-h.x=20;
+var g = { x: 10 };
+var h = g;
+h.x = 20;
 console.log(g)
 
 
-function cc(rd){
-    this.radius=rd;
+function cc(rd) {
+    this.radius = rd;
 }
-cc.prototype.ar=
-    function (){
-        return Math.PI*Math.pow(this.radius,2);
+cc.prototype.ar =
+    function () {
+        return Math.PI * Math.pow(this.radius, 2);
     };
-var mc=new cc(10);
+var mc = new cc(10);
 console.log(mc);
 console.log(mc.ar());
 
 
 
-var lc={
-    radius:10,
-    ga: function(){
-        return Math.PI*Math.pow(this.radius,2);
+var lc = {
+    radius: 10,
+    ga: function () {
+        return Math.PI * Math.pow(this.radius, 2);
     }
 };
 console.log(lc);
 console.log(lc.ga());
 
 
+var i = new Array;
+i[0] = 'a';
+i[1] = 1;
+i[2] = function (x) { return x + 1; };
+i[3] = { d: 'd' };
+console.log(i);
+console.log(i[2](i[1]));
+
+
+var j = []
