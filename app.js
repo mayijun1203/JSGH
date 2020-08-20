2c0379ac7c3fb8f3fa0d1dbc3bc107ae1aaa4839
Yijun Ma <mayijun1203@gmail.com>
2020-08-19 22:08:12 -0400

    update

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-// (function () {
-//     "use strict";
+(function () {
+    "use strict";
 
-//     angular.module('ma', [])
-//         .controller('mc', function ($scope) {
-//             $scope.sh = function () {
-//                 return "Hello"
-//             };
-//             $scope.name = '';
-//             $scope.val = 0;
-//             $scope.disp = function () {
-//                 var totvalue = calc($scope.name);
-//                 $scope.val = totvalue;
-//             };
-//             function calc(string) {
-//                 var stringval = 0;
-//                 for (var i = 0; i < string.length; i++) {
-//                     stringval += string.charCodeAt(i);
-//                 }
-//                 return stringval;
-//             };
-//         });
-// })();
+    angular.module('ma', [])
+        .controller('mc', function ($scope) {
+            $scope.name = '';
+
+            $scope.sh = function () {
+                return "Hello"
+            };
+            
+            $scope.val = 0;
+
+            $scope.msg=function(){
+                return 
+            };
+
+            $scope.disp = function () {
+                var totvalue = calc($scope.name);
+                $scope.val = totvalue;
+            };
+            function calc(string) {
+                var stringval = 0;
+                for (var i = 0; i < string.length; i++) {
+                    stringval += string.charCodeAt(i);
+                }
+                return stringval;
+            };
+
+            $scope.state='hungry';
+            $scope.feed= function(){
+                $scope.state='full';
+            };
+        });
+})();
 
 
 // (function () {
@@ -40,4 +52,4 @@
 
 
 // minify
-!function(){"use strict";function e(e,n){e.name="haha",e.upper=function(){var a=n("uppercase");e.name=a(e.name)}}angular.module("di",[]).controller("dc",e),e.$inject=["$scope","$filter"]}();
+// !function(){"use strict";function e(e,n){e.name="haha",e.upper=function(){var a=n("uppercase");e.name=a(e.name)}}angular.module("di",[]).controller("dc",e),e.$inject=["$scope","$filter"]}();
