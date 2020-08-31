8f95359c84497f5c6107dd7215316e430976cfdc
Yijun Ma <mayijun1203@gmail.com>
2020-08-31 11:47:48 -0400

    update

--- fullstack/js/script.js
+++ fullstack/js/script.js
@@ -1,11 +1,11 @@
-let name1='da';
-name1='ma';
-console.log(name1)
+// let name1='da';
+// name1='ma';
+// console.log(name1)
 
-const name2='da';
-console.log(name2)
+// const name2='da';
+// console.log(name2)
 
-function greet(name){
-    return 'Hello '+name;
-}
-console.log(greet('da'));
+// function greet(name){
+//     return 'Hello '+name;
+// }
+// console.log(greet('da'));
---
+++ grunt.js
@@ -0,0 +1,5 @@
+'use strict';
+
+moduel.exports=function (grunt){
+    
+}
